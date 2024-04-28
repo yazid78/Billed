05c35f12a09bcc0ca9a49dbf3b69f15ba2d0e210
Yazid <yazid780@outlook.fr>
2024-04-28 19:06:06 +0200

    ajout test unitaire sur méthode handleClickNewBill

--- Billed-app-FR-Front/src/__tests__/Bills.js
+++ Billed-app-FR-Front/src/__tests__/Bills.js
@@ -1,8 +1,10 @@
 /**
  * @jest-environment jsdom
  */
-
-import {screen, waitFor} from "@testing-library/dom"
+import Bills from "../containers/Bills.js"; // Ajoutez cette importation
+import {screen, waitFor , fireEvent} from "@testing-library/dom"
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom/extend-expect"; //jai ajouter joute en + 
 import BillsUI from "../views/BillsUI.js"
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH} from "../constants/routes.js";
@@ -13,7 +15,6 @@ import router from "../app/Router.js";
 describe("Given I am connected as an employee", () => {
   describe("When I am on Bills Page", () => {
     test("Then bill icon in vertical layout should be highlighted", async () => {
-
       Object.defineProperty(window, 'localStorage', { value: localStorageMock })
       window.localStorage.setItem('user', JSON.stringify({
         type: 'Employee'
@@ -26,11 +27,18 @@ describe("Given I am connected as an employee", () => {
       await waitFor(() => screen.getByTestId('icon-window'))
       const windowIcon = screen.getByTestId('icon-window')
       //to-do write expect expression
-      expect(windowIcon.classList.contains('active-icon')).toBe(true);
-
-
-
+      expect(windowIcon.classList.contains('active-icon')).toBe(true)
     })
+
+    
+    test("Then, no bills should be shown", () => {
+      document.body.innerHTML = BillsUI({ data: [] });
+      const bill = screen.queryByTestId("data-table");
+      expect(bill).toBeNull();
+    });
+ 
+    
+
     test("Then bills should be ordered from earliest to latest", () => {
       document.body.innerHTML = BillsUI({ data: bills })
       const dates = screen.getAllByText(/^(19|20)\d\d[- /.](0[1-9]|1[012])[- /.](0[1-9]|[12][0-9]|3[01])$/i).map(a => a.innerHTML)
@@ -40,3 +48,20 @@ describe("Given I am connected as an employee", () => {
     })
   })
 })
+
+describe("Bills container", () => {
+  describe("When i click on new bills the handleClickNewBill redirects", () => {
+    test("It should navigate to the NewBill page", () => {
+      const onNavigateMock = jest.fn();
+      const billsContainer = new Bills({
+        document,
+        onNavigate: onNavigateMock,
+        store: null, 
+        localStorage: null, });
+      billsContainer.handleClickNewBill();
+      expect(onNavigateMock).toHaveBeenCalledWith(ROUTES_PATH.NewBill);
+      expect(screen.queryByTestId("btn-new-bill")).not.toBeNull();
+    });
+  });
+});
+
--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -3,16 +3,36 @@
  */
 
 import { screen } from "@testing-library/dom"
+import "@testing-library/jest-dom/extend-expect";
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 
 
-describe("Given I am connected as an employee", () => {
+
+/* describe("Given I am connected as an employee", () => {
   describe("When I am on NewBill Page", () => {
     test("Then ...", () => {
       const html = NewBillUI()
       document.body.innerHTML = html
       //to-do write assertion
+      const fileInputElement = screen.queryByTestId('file');
+      expect(fileInputElement).toBeInTheDocument();
     })
   })
-})
+}) */
+
+describe("Given I am connected as an employee", () => {
+  describe("When I am on NewBill Page", () => {
+    test("Then it should display the file input element for uploading a receipt", () => {
+      // Render NewBillUI
+      const html = NewBillUI();
+      document.body.innerHTML = html;
+
+      // Vérifie la présence de l'élément pour télécharger le justificatif
+      const fileInputElement = screen.getByTestId('file');
+
+      // Assert
+      expect(fileInputElement).toBeInTheDocument();
+    });
+  });
+});
