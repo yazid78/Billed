2286debf4ed6b93bf4235844ed39f398c4f74993
Yazid <yazid780@outlook.fr>
2024-05-02 22:13:53 +0200

    ajout test POST

--- Billed-app-FR-Front/src/__tests__/NewBill.js
+++ Billed-app-FR-Front/src/__tests__/NewBill.js
@@ -27,7 +27,77 @@ describe("Given I am connected as an employee", () => {
       const mailIcon = screen.getByTestId("icon-mail");
       expect(mailIcon).toBeTruthy();
     });
-    
   });
+});
+
+
+
+
+//test intégration POST
+
+describe("NewBill integration test", () => {
+  let onNavigate;
+  let alertMock;
+  let submitFunctionMock;
+  beforeEach(() => {
+    const html = NewBillUI();
+    document.body.innerHTML = html;
+    const store = {
+      bills: jest.fn(() => ({
+        create: jest.fn(() => Promise.resolve({ fileUrl: "testUrl", key: "testKey" })),
+        update: jest.fn(() => Promise.resolve()),
+      })),
+    };
+    const localStorage = {
+      getItem: jest.fn(() => JSON.stringify({ email: "test@example.com" })),
+    };
+    onNavigate = jest.fn(); // Initialize onNavigate as a mock here
+
+    alertMock = jest.fn(); // Creating a mock function for alert
+    submitFunctionMock =jest.fn()
+    window.alert = alertMock; // Assigning the mock to window.alert
+
+    new NewBill({ document, onNavigate, store, localStorage });
+  });
+
+  test("Submit form with valid data", async () => {
+    const fileField = screen.getByTestId("file");
+    fireEvent.change(fileField, {
+      target: {
+        files: [new File(["testFileContent"], "testFile.txt", { type: "text/plain" })], // Format de fichier invalide
+      },
+    });
+    
+    const nameInput = screen.getByTestId("expense-name");
+    fireEvent.change(nameInput, { target: { value: "Vol Paris Londres" } });
+
+    const datePickerInput = screen.getByTestId("datepicker");
+    fireEvent.change(datePickerInput, { target: { value: "2024-05-01" } });
+
+    const amountInput = screen.getByTestId("amount");
+    fireEvent.change(amountInput, { target: { value: "348" } });
+
+    const vatInput = screen.getByTestId("vat");
+    fireEvent.change(vatInput, { target: { value: "70" } });
+
+    const pctInput = screen.getByTestId("pct");
+    fireEvent.change(pctInput, { target: { value: "20" } });
+
+    const commentaryInput = screen.getByTestId("commentary");
+    fireEvent.change(commentaryInput, { target: { value: "Test commentary" } });
+
+
+    const submitButton = screen.getByText("Envoyer")
+    const file = screen.getByTestId("file")
+    submitButton.onclick = submitFunctionMock; 
+    fireEvent.click(submitButton);
+    fireEvent.click(file)
+    expect(submitFunctionMock).toHaveBeenCalled(); 
+    expect(onNavigate).toHaveBeenCalledWith("#employee/bills");
+    expect(alertMock).toHaveBeenCalledWith("Seuls les fichiers .jpg, .jpeg, .png sont autorisés !");
+
+  });
+
+
 });
 
